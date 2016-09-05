0267e2ebb34fb60d3d4545ee03562a6852a98301
Camil Staps <info@camilstaps.nl>
2016-09-06 01:01:08 +0200

    Clearer training page

--- resources/views/trainer.blade.php
+++ resources/views/trainer.blade.php
@@ -45,25 +45,27 @@ use HebrewParseTrainer\Tense;
 @endsection
 
 @section('content')
-<div id="trainer-input-container">
-	<p class="bg-danger" id="trainer-404">There are no verbs matching the criteria in our database.</p>
-	<p class="lead"><span class="hebrew hebrew-large" id="trainer-verb"></span><span id="trainer-answer"></span></p>
-</div>
-<div id="trainer-input-fancy"></div>
-<div class="text-muted">
-	<div id="trainer-input-help">
-		<p>Parse the verb and enter the answer as described below. Press return. If your answer was correct and there are multiple possible parsings, an extra input field will appear. After the first incorrect answer or after entering all possible answers, you can continue to the next verb by pressing return once more.</p>
-		<p>
-			<strong>Stems</strong>: either use the full name or a significant beginning (i.e. <code>Q</code> for Qal but <code>Pi</code> for Piel rather than <code>P</code>).<br/>
-			<strong>Tenses</strong>: use the abbreviations <code>pf</code>, <code>ipf</code>, <code>coh</code>, <code>imp</code>, <code>jus</code>, <code>infcs</code>, <code>infabs</code>, <code>pta</code> and <code>ptp</code>.<br/>
-			<strong>Person</strong>: <code>1</code>, <code>2</code>, <code>3</code> or none (infinitives and participles).<br/>
-			<strong>Gender</strong>: <code>m</code>, <code>f</code> or none (infinitives).<br/>
-			<strong>Number</strong>: <code>s</code>, <code>p</code> or none (infinitives).
-		</p>
-		<p><strong>Examples</strong>: <code>Q pf 3ms</code>, <code>ni pta fp</code>, <code>pi infabs</code>.</p>
-		<p>You can also use the buttons to enter your answer. This is an experimental feature.</p>
+<div id="trainer">
+	<div id="trainer-input-container">
+		<p class="bg-danger" id="trainer-404">There are no verbs matching the criteria in our database.</p>
+		<p class="lead"><span class="hebrew hebrew-large" id="trainer-verb"></span><span id="trainer-answer"></span></p>
 	</div>
-	<button type="button" class="btn btn-default btn-xs" id="show-hide-help">Show help</button>
+	<div id="trainer-input-fancy"></div>
+	<div class="text-muted">
+		<div id="trainer-input-help">
+			<p>Parse the verb and enter the answer as described below. Press return. If your answer was correct and there are multiple possible parsings, an extra input field will appear. After the first incorrect answer or after entering all possible answers, you can continue to the next verb by pressing return once more.</p>
+			<p>
+				<strong>Stems</strong>: either use the full name or a significant beginning (i.e. <code>Q</code> for Qal but <code>Pi</code> for Piel rather than <code>P</code>).<br/>
+				<strong>Tenses</strong>: use the abbreviations <code>pf</code>, <code>ipf</code>, <code>coh</code>, <code>imp</code>, <code>jus</code>, <code>infcs</code>, <code>infabs</code>, <code>pta</code> and <code>ptp</code>.<br/>
+				<strong>Person</strong>: <code>1</code>, <code>2</code>, <code>3</code> or none (infinitives and participles).<br/>
+				<strong>Gender</strong>: <code>m</code>, <code>f</code> or none (infinitives).<br/>
+				<strong>Number</strong>: <code>s</code>, <code>p</code> or none (infinitives).
+			</p>
+			<p><strong>Examples</strong>: <code>Q pf 3ms</code>, <code>ni pta fp</code>, <code>pi infabs</code>.</p>
+			<p>You can also use the buttons to enter your answer. This is an experimental feature.</p>
+		</div>
+		<button type="button" class="btn btn-default btn-xs" id="show-hide-help">Show help</button>
+	</div>
 </div>
 
 <hr/>
