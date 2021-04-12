d1fd68aee5e45e19430c5bd2dad49cf66d74fca1
Camil Staps <info@camilstaps.nl>
2021-04-12 21:25:51 +0200

    Fix update that removed twbs/bootstrap from vendor

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,7 +3,7 @@ let mix = require('laravel-mix');
 mix
 	.js('resources/assets/js/app.js', 'public/js')
 	.js('resources/assets/js/moderators.js', 'public/js')
-	.copy('vendor/twbs/bootstrap/dist/css/bootstrap.min.css', 'public/css')
+	.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css')
 	.copy('resources/assets/css/hebrewparsetrainer.css', 'public/css')
 	.copy('resources/assets/css/fonts/EzraSIL.ttf', 'public/css/fonts')
 	.copy('resources/assets/audio', 'public/audio');
