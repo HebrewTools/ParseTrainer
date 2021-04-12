d0410c1b0e72730c9434db92d97dc7fb60fe7e5b
Camil Staps <info@camilstaps.nl>
2021-04-12 21:26:22 +0200

    Update to Laravel 5.7

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,9 @@
 let mix = require('laravel-mix');
 
 mix
-	.js('resources/assets/js/app.js', 'public/js')
-	.js('resources/assets/js/moderators.js', 'public/js')
+	.js('resources/js/app.js', 'public/js')
+	.js('resources/js/moderators.js', 'public/js')
 	.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css')
-	.copy('resources/assets/css/hebrewparsetrainer.css', 'public/css')
-	.copy('resources/assets/css/fonts/EzraSIL.ttf', 'public/css/fonts')
-	.copy('resources/assets/audio', 'public/audio');
+	.copy('resources/css/hebrewparsetrainer.css', 'public/css')
+	.copy('resources/css/fonts/EzraSIL.ttf', 'public/css/fonts')
+	.copy('resources/audio', 'public/audio');
