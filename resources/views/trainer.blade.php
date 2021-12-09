735c8fedae50558e9d90aaed16eba1d66b29527f
Camil Staps <info@camilstaps.nl>
2021-12-09 12:27:12 +0100

    Clarify help text

--- resources/views/trainer.blade.php
+++ resources/views/trainer.blade.php
@@ -56,18 +56,21 @@ use HebrewParseTrainer\Tense;
 	</div>
 	<div id="trainer-input-fancy"></div>
 	<div class="text-muted">
-		<div id="trainer-input-help">
-			<p>Parse the verb and enter the answer as described below. Press return. If your answer was correct and there are multiple possible parsings, an extra input field will appear. After the first incorrect answer or after entering all possible answers, you can continue to the next verb by pressing return once more.</p>
+		<div> <!--  id="trainer-input-help" -->
+			<p>Parse the verb and enter the answer as described below or using the buttons. Press return. If your answer was correct and there are multiple possible parsings, an extra input field will appear. After the first incorrect answer or after entering all possible answers, you can continue to the next verb by pressing return once more.</p>
 			<p>
-				<strong>Stems</strong>: either use the full name or a significant beginning (i.e. <code>Q</code> for Qal but <code>Pi</code> for Piel rather than <code>P</code>).<br/>
+				<strong>Stems</strong>: either use the full name or a significant beginning (i.e. <code>Q</code> for Qal and <code>N</code>, <code>Pi</code>, <code>Pu</code>, <code>Hit</code>, <code>Hip</code>, and <code>Hop</code> for the derived stems).<br/>
 				<strong>Tenses</strong>: use the abbreviations <code>pf</code>, <code>ipf</code>, <code>coh</code>, <code>imp</code>, <code>jus</code>, <code>infcs</code>, <code>infabs</code>, <code>ptc</code> and <code>ptcp</code>.<br/>
 				<strong>Person</strong>: <code>1</code>, <code>2</code>, <code>3</code> or none (infinitives and participles).<br/>
 				<strong>Gender</strong>: <code>m</code>, <code>f</code> or none (infinitives).<br/>
 				<strong>Number</strong>: <code>s</code>, <code>p</code> or none (infinitives).
 			</p>
-			<p><strong>Examples</strong>: <code>Q pf 3ms</code>, <code>ni ptc fp</code>, <code>pi infabs</code>.</p>
-			<p>A note about participles: the <code>ptcp</code> option is only for the passive participle of the qal. All other participles should be entered with <code>ptc</code> (including participles of the passive stems).</p>
-			<p>You can also use the buttons to enter your answer. This is an experimental feature.</p>
+			<p>Examples: <code>Q pf 3ms</code>, <code>ni ptc fp</code>, <code>pi infabs</code>.</p>
+			<h5>Notes:</h5>
+			<ul>
+				<li>There is no 'common' gender. Instead, enter the masculine and feminine forms separately. The <code>N/A</code> option is for infinitives.</li>
+				<li>The <code>ptcp</code> option is only for the passive participle of the qal. All other participles should be entered with <code>ptc</code> (including participles of the passive stems).</li>
+			</ul>
 		</div>
 		<button type="button" class="btn btn-default btn-xs" id="show-hide-help">Show help</button>
 	</div>
