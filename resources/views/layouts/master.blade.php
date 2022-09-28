3fdb2f8fbf0f46a3666289d73122838df48281b0
Camil Staps <info@camilstaps.nl>
2022-09-28 10:25:13 +0200

    Add viewport tag

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -33,6 +33,7 @@ if (Auth::check()) {
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<meta name="csrf-token" content="{{ csrf_token() }}"/>
 
 		<title>ParseTrainer</title>
